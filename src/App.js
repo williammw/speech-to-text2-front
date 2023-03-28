ec2305d7698ba36e821558e29e2f579f8900dac9
wwwwwmw <william.manwai@gmail.com>
2023-03-29 05:27:17 +0800

    rename

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <header className="bg-gray-800 py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-white">
-            Speech to Text Transcription
+            Speech to Text Transcription (vercel)
           </h1>
         </div>
       </header>
